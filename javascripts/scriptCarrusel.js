9d941f5e246a4d1cc3a93cc0efe26d63a3a124dd
luisherrera98 <luchoherrerafernadenz@gmail.com>
2021-01-22 20:31:54 -0300

    1

--- javascripts/scriptCarrusel.js
+++ javascripts/scriptCarrusel.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll('.carousel')
     M.Carousel.init(elementosCarousel, {
-        duration: 15,
+        duration: 60,
         dist: -100,
         shift: 5,
         padding: 5,
